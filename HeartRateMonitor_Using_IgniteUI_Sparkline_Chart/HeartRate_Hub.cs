e95673d305e5e672cf436a8f22f04ac490029bed
Sunny Sharma <skrajobra.2008@gmail.com>
2016-07-14 08:46:55 +0530

    initial commit

---
+++ HeartRateMonitor_Using_IgniteUI_Sparkline_Chart/HeartRate_Hub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+
+namespace HeartRateMonitor_Using_IgniteUI_Sparkline_Chart
+{
+
+    public class HeartRate_Hub : Hub
+    {
+        static Timer _timer;       
+        private Random _rnd = new Random();
+        public void PulseSimulator()
+        {
+            if (_timer == null)
+            {
+                _timer = new Timer(GeneratePulse, null, 3000, 1000);
+            }
+        }
+
+        public void GeneratePulse(object state)
+        {
+            var pulseRate = new { pulseRate =  _rnd.Next(60, 100) };
+
+            Clients.All.updatePulse(JsonConvert.SerializeObject(pulseRate));
+        }
+    }
+}
---
+++ HeartRateMonitor_Using_IgniteUI_Sparkline_Chart/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+namespace HeartRateMonitor_Using_IgniteUI_Sparkline_Chart
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.MapSignalR();
+        }
+    }
+}
